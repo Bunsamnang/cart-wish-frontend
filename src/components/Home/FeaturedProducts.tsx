e056a43183be4163fde610ffa5928612f0454305
Bunsamnang <bunsamnangk@gmail.com>
2025-01-23 16:12:06 +0700

    add loading state in featuredProduct

--- src/components/Home/FeaturedProducts.tsx
+++ src/components/Home/FeaturedProducts.tsx
@@ -1,11 +1,13 @@
+import { LoaderCircle } from "lucide-react";
 import useData, { Product } from "../../hooks/useData";
 import ProductCard from "../Products/ProductCard";
 
 const FeaturedProducts = () => {
-  const { data: featuredProducts, errorMsg } = useData<Product[]>(
-    "/products/featured",
-    10 * 60 * 60 * 1000
-  );
+  const {
+    data: featuredProducts,
+    errorMsg,
+    isLoading,
+  } = useData<Product[]>("/products/featured", 10 * 60 * 60 * 1000);
 
   console.log(featuredProducts);
 
@@ -17,18 +19,26 @@ const FeaturedProducts = () => {
       {errorMsg ? (
         <p className="text-center text-red-500">{errorMsg}</p>
       ) : (
-        <div className="flex justify-evenly max-sm:flex-col max-sm:items-center gap-5 max-lg:px-2">
-          {featuredProducts &&
-            featuredProducts.map((product, index) => (
-              <div
-                data-aos="fade-down"
-                data-aos-delay={`${index * 250}`}
-                key={product._id}
-              >
-                <ProductCard product={product} />
-              </div>
-            ))}
-        </div>
+        <>
+          {isLoading ? (
+            <div className="flex justify-center items-center h-screen">
+              <LoaderCircle className="animate-spin text-slate-800 w-10 h-10 " />
+            </div>
+          ) : (
+            <div className="flex justify-evenly max-sm:flex-col max-sm:items-center gap-5 max-lg:px-2">
+              {featuredProducts &&
+                featuredProducts.map((product, index) => (
+                  <div
+                    data-aos="fade-down"
+                    data-aos-delay={`${index * 250}`}
+                    key={product._id}
+                  >
+                    <ProductCard product={product} />
+                  </div>
+                ))}
+            </div>
+          )}
+        </>
       )}
     </section>
   );
